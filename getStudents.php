218c9ac073c388025d020146ffd6e5dbdf991608
Robert G. Lewis <lewisrg99@yahoo.com>
2018-02-21 14:20:43 -0500

    changes_stanged

--- getStudents.php
+++ getStudents.php
@@ -1,20 +1,24 @@
 <?php
-//$sSERVERNAME="localhost";
-//$sUSER="rlewis";
-//$sPASSWORD="L@wr3nce99";
-//$sDATABASE="contactapp";
 
+// ********Traditional Connection String ***********************************************************************//
+//$sSERVERNAME="????????";
+//$sUSER="?????";
+//$sPASSWORD="????????";
+//$sDATABASE="????????";
 //$con=mysqli_connect($sSERVERNAME,$sUSER,$sPASSWORD,$sDATABASE)or die("Error " . mysqli_error($connection));
+//$sql = "select studentID, fname, lname, email, phone from students";
+//$result = mysqli_query($con, $sql) or die("Error in Selecting " . mysqli_error($con));
+//while($rs = mysqli_fetch_assoc($result))
+//************************************************************************************************************** */
 
+
+//**********************************SQL LITE CONNECTION******************************************************** */
 $db =  new SQLite3('robbase');
 
-//$sql = "select studentID, fname, lname, email, phone from students";
-
 $result = $db->query('SELECT memID,lname,fname,phone from rgltable');
-//$result = mysqli_query($con, $sql) or die("Error in Selecting " . mysqli_error($con));
-  
+
  $outp = ""; 
- //while($rs = mysqli_fetch_assoc($result))
+ 
  while ($rs = $result->fetchArray())
  {
 	if ($outp != "") {$outp .= ",";}
@@ -27,5 +31,6 @@ $result = $db->query('SELECT memID,lname,fname,phone from rgltable');
   
     //mysqli_close($con);
 	
-	echo ($outp);
+    echo ($outp);
+//************************************************************************************************************** */
 ?>
